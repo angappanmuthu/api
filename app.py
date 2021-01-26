f3b52a5bbc263e324c6988a7bb9830afb65c54f5
angappanmuthu <44269293+angappanmuthu@users.noreply.github.com>
2021-01-26 19:45:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+print(str(datetime.now()).split(' '))
+
+
+@app.route('/')
+def home():
+    date_time = str(datetime.now()).split(' ')
+    date = date_time[0]
+    time = date_time[1].split('.')[0]
+    return jsonify({"date": date, "time": time})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
